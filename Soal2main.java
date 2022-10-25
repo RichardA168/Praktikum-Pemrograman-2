cdd7d6bff5af4d7b065c64ea993d37a98def40ed
RichardA168 <104208649+RichardA168@users.noreply.github.com>
2022-10-25 21:20:50 +0800

    Add files via upload

---
+++ Soal2main.java
@@ -0,0 +1,38 @@
+package praktikum3.soal2;
+
+import java.util.Scanner;
+import java.util.LinkedList;
+
+public class Soal2main {
+    static Scanner forPerson=new Scanner(System.in);
+    
+    public static Negara isiNegara(){
+        String namaNegara, jenisNegara, namaPemimpin;
+        int tglMerdeka, blnMerdeka, thnMerdeka;
+        namaNegara=forPerson.nextLine();
+        jenisNegara=forPerson.nextLine();
+        namaPemimpin=forPerson.nextLine();
+        if(Negara.negaraMonarki(jenisNegara)){
+            return new Negara(namaNegara, jenisNegara, namaPemimpin);
+        }
+        tglMerdeka=forPerson.nextInt(); forPerson.nextLine();
+        blnMerdeka=forPerson.nextInt(); forPerson.nextLine();
+        thnMerdeka=forPerson.nextInt(); forPerson.nextLine();
+        return new Negara(namaNegara, jenisNegara, namaPemimpin, tglMerdeka, blnMerdeka, thnMerdeka);
+    }
+    
+    
+    public static void main(String[] args) {
+        LinkedList<Negara> allNegara=new LinkedList<>();
+        int jumlahNegara;
+        jumlahNegara=forPerson.nextInt();
+        forPerson.nextLine();
+        for(int i=0;i<jumlahNegara;i++){
+            allNegara.add(isiNegara());
+        }
+        while(!allNegara.isEmpty()){
+            allNegara.poll().displayStatus();
+        }
+        forPerson.close();
+    }
+}
